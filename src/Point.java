57f47d9a9d0dec648a8a1abf79db6d268c93449c
Patrycja <patrycja.wanga@gmail.com>
2022-12-04 10:38:26 +0100

    Laboratorium 6 Zadanie 2

--- src/Point.java
+++ src/Point.java
@@ -1,2 +1,60 @@
-package PACKAGE_NAME;public class Point {
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+
+public class Point {
+    private final double X;
+    private final double Y;
+
+    public static final Point zeroZero=new Point(0.0 ,0.0);
+    public static final Point oneZero=new Point(1.0 ,0.0);
+
+
+    public Point (double x, double y){
+        this.X=x;
+        this.Y=y;
+    }
+
+    public double getX() {
+        return X;
+    }
+
+    public double getY() {
+        return Y;
+    }
+
+    Point move(double vx, double vy){
+        double newX = this.getX()+vx;
+        double newY = this.getY()+vy;
+
+        Point newPoint = new Point(newX, newY);
+        return newPoint;
+    }
+
+    Point symetryOx(){
+        double newX = this.getX();
+        double newY = -this.getY();
+        Point newPoint = new Point(newX, newY);
+        return newPoint;
+    }
+
+    Point symetryOy(){
+        double newX = -this.getX();
+        double newY = this.getY();
+        Point newPoint = new Point(newX, newY);
+        return newPoint;
+    }
+
+    Point rotate(int degree){
+        double newX = this.getX()*Math.cos(degree)-this.getY()*Math.sin(degree);
+        double newY = this.getX()*Math.sin(degree)+this.getY()*Math.cos(degree);
+        Point newPoint = new Point(newX, newY);
+        return newPoint;
+    }
+
+
+    @Override
+    public String toString() {
+        NumberFormat df = new DecimalFormat("#0.0");
+        return "("+df.format(getX())+", "+df.format(getY())+")";
+    }
 }
---
+++ src/main.java
@@ -0,0 +1,12 @@
+public class main {
+    public static void main(String[] args) {
+        System.out.println(Point.oneZero.toString());
+        System.out.println(Point.zeroZero.toString());
+        Point sample = new Point(3,4);
+        System.out.println(sample.move(3,0));
+        System.out.println(sample.symetryOx());
+        System.out.println(sample.symetryOy());
+        System.out.println(sample.rotate(40));
+    }
+
+}
