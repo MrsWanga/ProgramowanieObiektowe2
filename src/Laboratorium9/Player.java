95cb34e230875dc3bcb0c6ce4b56ab15d31ffd6c
Patrycja <patrycja.wanga@gmail.com>
2023-01-15 11:05:29 +0100

    Lab9Zad3

--- src/Laboratorium9/Player.java
+++ src/Laboratorium9/Player.java
@@ -1,9 +1,9 @@
 package Laboratorium9;
 
 public class Player extends TeamMember{
-    int number;
-    int age;
-    String position;
+    private int number;
+    private int age;
+    private String position;
 
     Player(String name,
            String surname,
@@ -12,5 +12,29 @@ public class Player extends TeamMember{
            int age,
            String position) {
         super(name, surname, pesel);
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
     }
 }
