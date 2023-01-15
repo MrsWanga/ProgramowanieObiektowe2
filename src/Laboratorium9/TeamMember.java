6353e6bc7609762d3e72b512b6b81e229ee8a1f2
Patrycja <patrycja.wanga@gmail.com>
2023-01-15 10:52:55 +0100

    Lab9Zad1

---
+++ src/Laboratorium9/TeamMember.java
@@ -0,0 +1,51 @@
+package Laboratorium9;
+
+import java.util.Objects;
+
+public abstract class TeamMember {
+    String name;
+    String surname;
+    String pesel;
+
+    TeamMember(String name, String surname, String pesel){
+        this.name=name;
+        this.surname=surname;
+        this.pesel=pesel;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getPesel() {
+        return pesel;
+    }
+
+    public void setPesel(String pesel) {
+        this.pesel = pesel;
+    }
+
+    public int compareTo(TeamMember tm){
+        return this.getPesel().compareTo(tm.getPesel());
+    }
+
+    public boolean equals(TeamMember tm){
+       return this.getPesel().equals(tm.getPesel());
+    }
+
+    public int hashCode() {
+        return Objects.hash(pesel);
+    }
+
+}
